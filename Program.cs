a996b88350f6b5633d1c504c2cece3bd6d8ed28c
Binh Do <binhdothanh.navn@gmail.com>
2023-05-31 22:49:45 +0700

    update

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ var command = "echo Hello World";
 Console.WriteLine("Start Command " + command);
 using (Process proc = new Process())
 {
-    proc.StartInfo.FileName = "/bin sh";
+    proc.StartInfo.FileName = "/bin/sh";
     proc.StartInfo.Arguments = "-c \" " + command + " \"";
     proc.StartInfo.UseShellExecute = false; 
     proc.Start();
